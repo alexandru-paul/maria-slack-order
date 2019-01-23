95f876174f0cf09357308ef2ea5291d4bcd72fde
Alex Paul <paul.d.alexandru@gmail.com>
2019-01-23 16:50:49 +0200

    added avatar and username name display

--- src/App.js
+++ src/App.js
@@ -2,7 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 
 class PostMessage extends Component {
-  getQueryString() {
+  buildUrl(url, parameters) {
+    let qs = "";
+    for (const key in parameters) {
+        if (parameters.hasOwnProperty(key)) {
+            const value = parameters[key];
+            qs +=
+                encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+        }
+    }
+    if (qs.length > 0) {
+        qs = qs.substring(0, qs.length - 1); //chop off last "&"
+        url = url + "?" + qs;
+    }
+
+    return url;
+}
+
+  postMessage = (e) => {
+    e.preventDefault();
+    let getprm = this.props.getQueryString('access_token');
+    let fetchUrl = this.buildUrl('https://slack.com/api/chat.postMessage',{
+                    token : getprm,
+                    channel :'GFLS780EN',
+                    text : 'test from oauth app',
+                    as_user : 'true',
+                  });
+    fetch(fetchUrl).then(response => response.json()).then(data => console.log(data));
+  }
+
+  render() {
+    return (
+      <div className="post-message-wrapper">
+        <input type="text" className="slack-msg"/>
+        <a href="#" className="send-msg" onClick={this.postMessage}>Send Messnge</a>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      avatar: '',
+    }
+  }
+
+  getQueryString(param) {
     let getParams = {};
     if(document.location.toString().indexOf('?') !== -1) {
         var query = document.location
@@ -19,43 +67,29 @@ class PostMessage extends Component {
         }
     }
     
-    return getParams;
+    return getParams[param];
   }
 
-  postMessage= (e) => {
-    e.preventDefault();
-    let getprm = this.getQueryString();
-    fetch('https://slack.com/api/chat.postEphemeral', {
-      method: 'POST',
-      body: JSON.stringify({
-        token:getprm['access_token'],
-        channel :'GFLS780EN',
-        text : 'test from oauth app',
-        as_user : 'true',
-      })
-    }).then(response => console.log(response.json()));
-  }
-
-  render() {
-    return (
-      <div className="post-message-wrapper">
-        <input type="text"/>
-        <a href="#" className="send-msg" onClick={this.postMessage}>Send Messnge</a>
-      </div>
-    );
-  }
-}
-
-class App extends Component {
   componentDidMount() {
-    
+    let getUserDataUrl = 'https://slack.com/api/users.profile.get?token='+ this.getQueryString('access_token');
+    fetch(getUserDataUrl).then(response => response.json()).then((data) => {
+      this.setState({
+        username: data.profile.first_name,
+        avatar: data.profile.image_192,
+      });
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Maria Order App React</h1>
-        <PostMessage/>
+        {this.state.username &&
+          <div className="welcome-bar">
+            <img src={this.state.avatar} />
+            <h1>Hello {this.state.username}, and welcome to the Maria Order App</h1>
+          </div>
+        }
+        <PostMessage getQueryString={this.getQueryString}/>
       </div>
     );
   }
