3831d4221198fef90f8f1145b61f8e739cc298d9
Alex Paul <paul.d.alexandru@gmail.com>
2019-01-23 14:15:14 +0200

    added fetch function for handling posting the messages

--- src/App.js
+++ src/App.js
@@ -2,17 +2,58 @@ import React, { Component } from 'react';
 import './App.css';
 
 class PostMessage extends Component {
+  getQueryString() {
+    let getParams = {};
+    if(document.location.toString().indexOf('?') !== -1) {
+        var query = document.location
+                      .toString()
+                      // get the query string
+                      .replace(/^.*?\?/, '')
+                      // and remove any existing hash string (thanks, @vrijdenker)
+                      .replace(/#.*$/, '')
+                      .split('&');
+
+        for(var i=0, l=query.length; i<l; i++) {
+          var aux = decodeURIComponent(query[i]).split('=');
+          getParams[aux[0]] = aux[1];
+        }
+    }
+    
+    return getParams;
+  }
+
+  postMessage= (e) => {
+    e.preventDefault();
+    let getprm = this.getQueryString();
+    fetch('https://slack.com/api/chat.postEphemeral', {
+      method: 'POST',
+      headers: {
+        'Authorization':'Bearer ' + getprm['access_token'],
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        channel :'U0MMW23Q9',
+        text : 'test from oauth app',
+        as_user : 'true',
+      })
+    }).then(response => console.log(response.json()));
+  }
+
   render() {
     return (
       <div className="post-message-wrapper">
         <input type="text"/>
-        <a href="#" className="send-msg">Send Messnge</a>
+        <a href="#" className="send-msg" onClick={this.postMessage}>Send Messnge</a>
       </div>
     );
   }
 }
 
 class App extends Component {
+  componentDidMount() {
+    
+  }
+
   render() {
     return (
       <div className="App">
