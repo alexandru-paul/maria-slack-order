11b0be89c8f5bed9d700d927387e6128f5e8120f
Alex Paul <paul.d.alexandru@gmail.com>
2019-01-23 13:20:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class PostMessage extends Component {
+  render() {
+    return (
+      <div className="post-message-wrapper">
+        <input type="text"/>
+        <a href="#" className="send-msg">Send Messnge</a>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Maria Order App React</h1>
+        <PostMessage/>
       </div>
     );
   }
