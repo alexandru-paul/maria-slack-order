3418ad89b3cfc613e41031da2f83eed10d5e51fd
Alex Paul <paul.d.alexandru@gmail.com>
2019-02-20 13:54:28 +0200

    refactored the call for mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -19,14 +19,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                      // you can specify a publicPath here
-                      // by default it use publicPath in webpackOptions.output
-                      publicPath: '../'
-                    }
-                },
+                MiniCssExtractPlugin.loader,
                 "css-loader"
                 ]
             }
